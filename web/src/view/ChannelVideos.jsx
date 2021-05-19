bb46859703546a07b0348b089f6abeb4be6ea3ee
Timmy_DESK <timmy85618@gmail.com>
2021-05-19 16:27:20 +0800

    test

--- web/src/view/ChannelVideos.jsx
+++ web/src/view/ChannelVideos.jsx
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { useParams,Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 /*
     1.點頭像
     2.顯示頻道影片
     3.
 */
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(5),
+    margin: "auto",
+    maxWidth: "auto",
+  },
+  image: {
+    width: "auto",
+    height: "auto",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+
 const ChannelVideos = () => {
   let { channelId } = useParams();
   return <YtPic channelId={channelId} />;
@@ -31,15 +55,18 @@ const YtPic = props => {
     setDate([...new Set(channelrecentinfo.map(data => data.date))]);
   }, [channelrecentinfo]);
 
+  console.log(channelrecentinfo);
   return (
-    <Grid container>
-      {date.map(date => (
-        <Pictures
-          key={date}
-          date={date}
-          video={channelrecentinfo.filter(data => data.date === date)}
-        />
-      ))}
+    <Grid item container>
+      <Grid container item spacing={2}>
+        {date.map(date => (
+          <Pictures
+            key={date}
+            date={date}
+            video={channelrecentinfo.filter(data => data.date === date)}
+          />
+        ))}
+      </Grid>
     </Grid>
   );
 };
@@ -48,34 +75,37 @@ YtPic.propTypes = {
 };
 
 const Pictures = props => {
+  const classes = useStyles();
   const { date, video } = props;
   let { channelId } = useParams();
-  console.log(date);
-  console.log(video);
+  //console.log(date);
+  //console.log(video);
   return (
-    <Grid item container direction="column" alignItems="center">
-      <Grid item>{date}</Grid>
+    <Grid item container>
       <Grid item>
-        {video.map((video, index) => {
-          console.log(video.videoid);
-          return (
-            <Grid item container key={index} direction="column">
-              <Grid item>{video.title}</Grid>
-              <Grid item>
-                <Link
-                  to={{
-                    pathname: `/MainPage/ChannelVideos/${channelId}/${video.videoid}`,
-                  }}
-                >
-                  <ButtonBase>
-                    <img alt="complex" src={video.imgurl} />
-                  </ButtonBase>
-                </Link>
-              </Grid>
-            </Grid>
-          );
-        })}
+        <Typography variant="h4">{date}</Typography>
       </Grid>
+      {video.map((video, index) => {
+        console.log(video.videoid);
+        return (
+          <Grid item container key={index} direction="column" alignItems="center">
+            <Grid item>
+              <Link
+                to={{
+                  pathname: `/MainPage/ChannelVideos/${channelId}/${video.videoid}`,
+                }}
+              >
+                <ButtonBase className={classes.image}>
+                  <img alt="complex" src={video.imgurl} />
+                </ButtonBase>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">{video.title}</Typography>
+            </Grid>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
@@ -83,6 +113,5 @@ Pictures.propTypes = {
   date: PropTypes.string.isRequired,
   video: PropTypes.array.isRequired,
 };
-
 
 export default ChannelVideos;
